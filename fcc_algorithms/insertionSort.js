3a5302123ac22ceb77bc0614a7d242ea394e8f38
SamLind11 <samuellind11@gmail.com>
2024-01-03 16:58:46 -0500

    start on insertionSort.js

---
+++ fcc_algorithms/insertionSort.js
@@ -0,0 +1,35 @@
+/*
+This method works by building up a sorted array at the beginning of the list. It
+begins the sorted array with the first element. Then it inspects the next
+element and swaps it backwards into the sorted array until it is in sorted
+position. It continues iterating through the list and swapping new items
+backwards into the sorted portion until it reaches the end. This algorithm has 
+quadratic time complexity in the average and worst cases.
+*/
+
+function insertionSort(array) {
+    let length = array.length;
+    
+    if (length == 0 || length == 1) {
+        return array;
+    }
+    let to_sort;
+    for (let i = 1; i < length; i++) {
+
+        to_sort = array[i];
+        let tracker = i;
+        while (tracker > 0 && to_sort >= array[tracker]) {
+            tracker--;
+        }
+        if (tracker != i) {
+            array = array.splice(i, 1); // Delete 1 element at index i.
+            array = array.splice(tracker + 1, 0, to_sort);  // Insert to_sort 
+        }
+    }
+    return array;
+    
+}
+
+let test = [1, 5, 3, 4, 2];
+
+console.log(insertionSort(test));
